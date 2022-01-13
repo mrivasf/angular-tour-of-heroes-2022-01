642c3643320bd50842f18ed28c5e2a0ad53e5848
Javier <javier@eskura21.com>
2022-01-13 20:00:50 +0100

    HeroService

---
+++ src/app/hero.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
+
+  // private heroService: HeroService;
+
+  // this.heroService = new HeroService();
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes() {
+    this.heroService.getHeroes().subscribe(heroesRecibidos => this.heroes = heroesRecibidos);
+
+    // this.heroService.getHeroes().subscribe(this.recepcionHeroes.bind(this));
+  }
+
+  // recepcionHeroes(heroesRecibidos: Hero[]) {
+  //   console.log('this vale',this);
+  //   this.heroes = heroesRecibidos;
+  // }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
